01ae8e0753c8350e7994c7e8d1705cbcef926c78
Manuel Barbieri <barbieripariani@gmail.com>
2026-05-23 20:27:52 -0300

    Actualizo el controller de Faculty para el alta de facultades

--- src/main/java/com/is1/proyecto/controllers/FacultyController.java
+++ src/main/java/com/is1/proyecto/controllers/FacultyController.java
@@ -2,6 +2,7 @@ package com.is1.proyecto.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import com.is1.proyecto.models.Faculty;
 
 import spark.ModelAndView;
@@ -12,6 +13,60 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class FacultyController {
 
     public static void init() {
+
+        get("/faculty/create", (req, res) -> {
+            String role = req.session().attribute("role");
+            if (role == null || !role.equals("admin")) {
+                res.redirect("/dashboard?error=No tiene permisos de administrador.");
+                return null;
+            }
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("faculties", Faculty.findAll().toMaps());
+
+            String successMessage = req.queryParams("message");
+            if (successMessage != null)
+                model.put("successMessage", successMessage);
+            String errorMessage = req.queryParams("error");
+            if (errorMessage != null)
+                model.put("errorMessage", errorMessage);
+
+            return new ModelAndView(model, "faculty.mustache");
+        }, new MustacheTemplateEngine());
+
+        post("faculty/create", (req, res) -> {
+            String role = req.session().attribute("role");
+            if (role == null || !role.equals("admin")) {
+                res.redirect("/?error=No tienes permiso para realizar esta accion.");
+                return null;
+            }
+
+            String name = req.queryParams("name");
+
+            if (name == null || name.isEmpty()) {
+                res.redirect("/faculty/create?error=Faltan campos obligatorios.");
+                return null;
+            }
+
+            try {
+                if (Faculty.findFirst("name = ?", name) != null) {
+                    res.redirect("/faculty/create?error=La facultad ya está registrada.");
+                    return null;
+                }
+
+                Faculty newFaculty = new Faculty();
+                newFaculty.set("name", name);
+                newFaculty.saveIt();
+
+                res.redirect("/dashboard?message=Facultad creada con exito.");
+                return "";
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.redirect("/faculty/create?error=Se produjo un error al crear la facultad.");
+                return "";
+            }
+        });
+
         // Mostramos el formulario de baja
         get("/faculty/delete", (req, res) -> {
             String role = req.session().attribute("role");
