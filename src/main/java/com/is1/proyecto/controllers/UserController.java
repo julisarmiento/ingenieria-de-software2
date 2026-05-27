063eb0eff8381d048f1d469e83699a3c33dcad27
tomas-monzon <tomasmonzon8@gmail.com>
2026-05-27 20:51:44 -0300

    Cree StudentService y modifique UserController

--- src/main/java/com/is1/proyecto/controllers/UserController.java
+++ src/main/java/com/is1/proyecto/controllers/UserController.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.is1.proyecto.exceptions.UserAlreadyExistsException;
 import com.is1.proyecto.exceptions.ValidationException;
-import com.is1.proyecto.services.UserService;
+import com.is1.proyecto.services.StudentService;
+
+import java.nio.charset.StandardCharsets;
 
 import spark.ModelAndView;
 import static spark.Spark.get;
@@ -47,7 +49,7 @@ public class UserController {
         post("/user/create", (req, res) -> {
             String username = req.queryParams("username"); 
             String password = req.queryParams("password");
-            
+
             String name = req.queryParams("name");
             String surname = req.queryParams("surname");
             String dni = req.queryParams("dni");
@@ -71,43 +73,20 @@ public class UserController {
                 res.redirect("/user/create?error=Todos los campos son obligatorios.");
                 return ""; // Retorna una cadena vacía ya que la respuesta ya fue redirigida.
             }
-            UserService service = new UserService();
+            StudentService service = new StudentService();
 
             try {
-               service.registerUser(name, password);
+               service.registerStudent(username, password, name, surname, dni, mail, ageStr, phoneNum);
                 res.status(201); // Código de estado HTTP 201 (Created) para una creación exitosa.
-                res.redirect("/user/create?message=" + java.net.URLEncoder.encode("Cuenta creada exitosamente para " + name + "!", java.nio.charset.StandardCharsets.UTF_8));
+                res.redirect("/user/create?message=" + java.net.URLEncoder.encode("Cuenta creada exitosamente para " + name + "!", StandardCharsets.UTF_8));
                 return ""; 
 
-                ac.set("name", username); // Asigna el nombre de usuario.
-                ac.set("password", hashedPassword); // Asigna la contraseña hasheada.
-                ac.set("role", "estudiante");
-                ac.saveIt(); // Guarda el nuevo usuario en la tabla 'users'.
-
-                int userId = ac.getInteger("id");
-
-                //Insercion en estudiante
-                Student s = new Student();
-                s.set("id", userId); // Lo vinculamos usando el mismo ID
-                s.set("name", name);
-                s.set("surname", surname);
-                s.set("dni", dni);
-                s.set("mail", mail);
-                s.set("age", Integer.parseInt(ageStr)); // Convertimos la edad a número entero
-                s.set("phoneNum", phoneNum); 
-                s.set("isFreshman", true);
-                s.insert();
-
-                res.status(201); // Código de estado HTTP 201 (Created) para una creación exitosa.
-                // Redirige al formulario de creación con un mensaje de éxito.
-                res.redirect("/user/create?message=Cuenta creada exitosamente para " + name + "!");
-                return ""; // Retorna una cadena vacía.
             } catch (ValidationException e) {
-                res.redirect("/user/create?error=" + java.net.URLEncoder.encode(e.getMessage(), java.nio.charset.StandardCharsets.UTF_8));
+                res.redirect("/user/create?error=" + java.net.URLEncoder.encode(e.getMessage(), StandardCharsets.UTF_8));
                 return ""; 
 
             }catch (UserAlreadyExistsException e) {
-                res.redirect("/user/create?error=" + java.net.URLEncoder.encode(e.getMessage(), java.nio.charset.StandardCharsets.UTF_8));
+                res.redirect("/user/create?error=" + java.net.URLEncoder.encode(e.getMessage(), StandardCharsets.UTF_8));
                 return ""; 
 
             } catch (Exception e) {
