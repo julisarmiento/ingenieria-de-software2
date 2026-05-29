e8108979bd5800d6d14d34c0aa1ddeac39a4fe06
Juliana Sarmiento <julianasarmiento11@gmail.com>
2026-05-28 21:34:49 -0300

    Implemente transacciones

--- src/main/java/com/is1/proyecto/services/ProfessorService.java
+++ src/main/java/com/is1/proyecto/services/ProfessorService.java
@@ -1,7 +1,6 @@
 package com.is1.proyecto.services;
-
+import org.javalite.activejdbc.Base;
 import org.mindrot.jbcrypt.BCrypt;
-
 import com.is1.proyecto.models.Professor;
 import com.is1.proyecto.models.User;
 
@@ -45,38 +44,60 @@ public class ProfessorService {
         String last4 = dni.substring(dni.length() - 4);
         String hashedPassword = BCrypt.hashpw(last4, BCrypt.gensalt());
 
-        // insercion para user
-        User newUser = new User();
-        newUser.set("name", username);
-        newUser.set("password", hashedPassword);
-        newUser.set("role", "professor");
-        newUser.saveIt();
 
-        int userId = newUser.getInteger("id");
+        try{
+            Base.openTransaction();
+            User newUser = new User();
+            newUser.set("name", username);
+            newUser.set("password", hashedPassword);
+            newUser.set("role", "professor");
+            newUser.saveIt();
 
-        // insercion para profesor
-        Professor prof = new Professor();
-        prof.set("id", userId);
-        prof.set("name", nombre);
-        prof.set("surname", apellido);
-        prof.set("mail", correo);
-        prof.set("dni", dni);
-        prof.insert();
+            int userId = newUser.getInteger("id");
+
+        
+            Professor prof = new Professor();
+            prof.set("id", userId);
+            prof.set("name", nombre);
+            prof.set("surname", apellido);
+            prof.set("mail", correo);
+            prof.set("dni", dni);
+            prof.insert();
+            
+            Base.commitTransaction();
+        }catch(Exception e){
+            Base.rollbackTransaction();
+            throw new RuntimeException("Error al crear profesor: " + e.getMessage(), e);
+        }
     }
 
-        // Creamos un método que reciba los parámetros necesarios
+    
     public String deleteProfessor(String id) {
 
-        User newUser = User.findFirst("id = ?", id);
+       
         Professor prof = Professor.findFirst("id = ?", id);
+        if(prof == null){
+            throw new IllegalArgumentException("El profesor no existe");
+        }
+
+        User user = User.findFirst("id = ?", id);
         String nameProfessor = prof.getString("name");
+
+        try{
+            Base.openTransaction();
+            
         
-        if(newUser != null && prof != null){
-            newUser.delete();
+            if(user != null){
+                user.delete();
+            }
+        
             prof.delete();
+            Base.commitTransaction();
             return nameProfessor;
-        } else {
-            return null;
+
+        }catch(Exception e){
+            Base.rollbackTransaction();
+            throw new RuntimeException("Error al eliminar al profesor", e);
         }
 
     }
