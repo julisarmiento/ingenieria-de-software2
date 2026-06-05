e52f6ee8f7da4ccfdc3e1225bd635370abc4ae9c
Manuel Barbieri <barbieripariani@gmail.com>
2026-06-05 14:06:14 -0300

    Completo la inscripción a materias (necesita fix's)

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -1,10 +1,10 @@
 -- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
 DROP TABLE IF EXISTS finalNotes;
-DROP TABLE IF EXISTS enrollments
+DROP TABLE IF EXISTS enrollments;
 DROP TABLE IF EXISTS periods;
 DROP TABLE IF EXISTS prerequisites;
 DROP TABLE IF EXISTS planSubjects;
-DROP TABLE IF EXISTS student_programs
+DROP TABLE IF EXISTS student_programs;
 DROP TABLE IF EXISTS programOfStudies;
 DROP TABLE IF EXISTS subjects;
 DROP TABLE IF EXISTS careers;
