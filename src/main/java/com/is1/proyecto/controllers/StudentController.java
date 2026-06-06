8552fbafb05193c2b94034cb2ab0a2642c2954d1
Manuel Barbieri <barbieripariani@gmail.com>
2026-06-05 22:16:09 -0300

    Fix, no se veian las materias al intentar inscribir a un alumno

--- src/main/java/com/is1/proyecto/controllers/StudentController.java
+++ src/main/java/com/is1/proyecto/controllers/StudentController.java
@@ -2,7 +2,9 @@ package com.is1.proyecto.controllers;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.javalite.activejdbc.LazyList;
@@ -13,6 +15,7 @@ import com.is1.proyecto.models.PlanSubject;
 import com.is1.proyecto.models.Role;
 import com.is1.proyecto.models.Student;
 import com.is1.proyecto.models.StudentProgram;
+import com.is1.proyecto.models.Subject;
 import com.is1.proyecto.services.EnrollmentService;
 import com.is1.proyecto.services.StudentService;
 
@@ -152,9 +155,19 @@ public class StudentController {
             if (sp != null) {
                 Integer programId = sp.getInteger("program_of_study_id");
 
-                LazyList<PlanSubject> materiasDisponibles = PlanSubject.where("programOfStudy_id = ?", programId);
+                LazyList<PlanSubject> planSubjects = PlanSubject.where("programOfStudy_id = ?", programId);
 
-                model.put("materias", materiasDisponibles.toMaps());
+                List<Map<String, Object>> materiasDisponibles = new ArrayList<>();
+                for (PlanSubject ps : planSubjects) {
+                    Map<String, Object> dato = new HashMap<>(ps.toMap());
+                    Subject subject = Subject.findById(ps.getInteger("subject_id"));
+                    if (subject != null) {
+                        dato.put("name", subject.getString("name"));
+                    }
+                    materiasDisponibles.add(dato);
+                }
+
+                model.put("materias", materiasDisponibles);
             }
 
             String errorMessage = req.queryParams("errorMessage");
