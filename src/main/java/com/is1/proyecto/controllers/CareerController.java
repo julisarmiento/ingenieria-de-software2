52f9274a70ef9e97bd8369ee2e8257e19d963e0a
Manuel Barbieri <barbieripariani@gmail.com>
2026-06-07 13:06:45 -0300

    Fix final de la inscripción a materias

--- src/main/java/com/is1/proyecto/controllers/CareerController.java
+++ src/main/java/com/is1/proyecto/controllers/CareerController.java
@@ -1,12 +1,14 @@
 package com.is1.proyecto.controllers;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.is1.proyecto.models.Career;
 import com.is1.proyecto.models.Faculty;
-
+import com.is1.proyecto.models.Role;
 import com.is1.proyecto.services.CareerService;
 import com.is1.proyecto.services.StudentService;
 
@@ -14,9 +16,6 @@ import spark.ModelAndView;
 import static spark.Spark.get;
 import static spark.Spark.post;
 import spark.template.mustache.MustacheTemplateEngine;
-import com.is1.proyecto.models.Role; 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class CareerController {
 
@@ -53,16 +52,15 @@ public class CareerController {
                 return null;
             }
 
-            
             String name = req.queryParams("nombre_carrera");
-            String facultyId = req.queryParams("identificador_facultad"); 
+            String facultyId = req.queryParams("identificador_facultad");
 
             try {
                 service.createCareer(name, facultyId);
-                String mensajeCodificado = URLEncoder.encode("Carrera " + name + 
-                    " creada con exito.", StandardCharsets.UTF_8); //Codificamos el mensaje en 
-                                                                   //caso de que el nombre de la 
-                                                                   //carrera llegase a tener acentos
+                String mensajeCodificado = URLEncoder.encode("Carrera " + name +
+                        " creada con exito.", StandardCharsets.UTF_8); // Codificamos el mensaje en
+                                                                       // caso de que el nombre de la
+                                                                       // carrera llegase a tener acentos
                 res.redirect("/dashboard?message=" + mensajeCodificado);
                 return "";
 
@@ -75,7 +73,7 @@ public class CareerController {
 
         get("/career/delete", (req, res) -> {
             Role role = req.session().attribute("role");
-            if (role != Role.ADMIN)  {
+            if (role != Role.ADMIN) {
                 res.redirect("/?error=No tienes permiso para acceder a esta pagina.");
                 return null;
             }
@@ -104,7 +102,7 @@ public class CareerController {
 
         post("/career/delete", (req, res) -> {
             Role role = req.session().attribute("role");
-            if (role != Role.ADMIN)  {
+            if (role != Role.ADMIN) {
                 res.redirect("/?error=No tienes permiso para acceder a esta pagina.");
                 return null;
             }
@@ -124,58 +122,56 @@ public class CareerController {
         });
 
         get("/career/select", (req, res) -> {
-    Role role = req.session().attribute("role");
-    if (role != Role.ESTUDIANTE) {
-        res.redirect("/?error=No tienes permiso para acceder a esta pagina.");
-        return null;
+            Role role = req.session().attribute("role");
+            if (role != Role.ESTUDIANTE) {
+                res.redirect("/?error=No tienes permiso para acceder a esta pagina.");
+                return null;
+            }
+
+            Map<String, Object> model = new HashMap<>();
+
+            String successMessage = req.queryParams("message");
+            if (successMessage != null && !successMessage.isEmpty()) {
+                model.put("successMessage", successMessage);
+            }
+            String errorMessage = req.queryParams("error");
+            if (errorMessage != null && !errorMessage.isEmpty()) {
+                model.put("errorMessage", errorMessage);
+            }
+
+            model.put("careers", Career.findAll().toMaps());
+
+            return new ModelAndView(model, "career_select.mustache");
+        }, new MustacheTemplateEngine());
+
+        post("/career/select", (req, res) -> {
+            Boolean loggedIn = req.session().attribute("loggedIn");
+            Role role = req.session().attribute("role");
+            if (loggedIn == null || !loggedIn || role != Role.ESTUDIANTE) {
+                res.redirect("/?error=No tienes permiso para acceder a esta pagina.");
+                return null;
+            }
+
+            int studentId = req.session().attribute("userId");
+            int careerId = Integer.parseInt(req.queryParams("career_id"));
+            try {
+                StudentService stService = new StudentService();
+                stService.assignCareer(studentId, careerId);
+                String mensaje = URLEncoder.encode("Carrera asignada con exito.", StandardCharsets.UTF_8);
+                res.redirect("/dashboard?message=" + mensaje);
+                return "";
+
+            } catch (IllegalArgumentException e) {
+                String error = URLEncoder.encode(e.getMessage(), StandardCharsets.UTF_8);
+                res.redirect("/career/select?error=" + error);
+                return "";
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.redirect("/career/select?error=Error inesperado al asignar la carrera.");
+                return "";
+            }
+        });
     }
-
-    Map<String, Object> model = new HashMap<>();
-
-    String successMessage = req.queryParams("message");
-    if (successMessage != null && !successMessage.isEmpty()) {
-        model.put("successMessage", successMessage);
-    }
-    String errorMessage = req.queryParams("error");
-    if (errorMessage != null && !errorMessage.isEmpty()) {
-        model.put("errorMessage", errorMessage);
-    }
-
-    model.put("careers", Career.findAll().toMaps());
-
-    return new ModelAndView(model, "career_select.mustache");
-}, new MustacheTemplateEngine());
-
-post("/career/select", (req, res) -> {
-    Boolean loggedIn = req.session().attribute("loggedIn");
-    Role role = req.session().attribute("role");
-    if (loggedIn == null || !loggedIn || role != Role.ESTUDIANTE) {
-        res.redirect("/?error=No tienes permiso para acceder a esta pagina.");
-        return null;
-    }
-
-    int studentId = req.session().attribute("userId");
-    int careerId = Integer.parseInt(req.queryParams("career_id"));
-    try {
-        StudentService stService = new StudentService();
-        stService.assignCareer(studentId, careerId);
-        String mensaje = URLEncoder.encode("Carrera asignada con exito.", StandardCharsets.UTF_8);
-        res.redirect("/dashboard?message=" + mensaje);
-        return "";
-
-    } catch (IllegalArgumentException e) {
-        String error = URLEncoder.encode(e.getMessage(), StandardCharsets.UTF_8);
-        res.redirect("/career/select?error=" + error);
-        return "";
-
-    } catch (Exception e) {
-        e.printStackTrace();
-        res.redirect("/career/select?error=Error inesperado al asignar la carrera.");
-        return "";
-    }
-    });
-}
-
-    
 
 }
