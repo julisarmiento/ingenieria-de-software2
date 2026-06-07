cff8e945faaf9177abc86e1274ffa83ba319287a
tomas-monzon <tomasmonzon8@gmail.com>
2026-06-07 14:35:10 -0300

    Reinsercion de archivo EmailService

---
+++ src/main/java/com/is1/proyecto/services/EmailService.java
@@ -0,0 +1,51 @@
+package com.is1.proyecto.services;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class EmailService {
+    
+    private static final String REMITENTE = "confimarcioncuentaprofesor@gmail.com"; 
+    private static final String PASSWORD_APP = "vadvxmhwdqiyxvbf";
+
+    public static void enviarCorreoConfirmacion(String id, String correo_destinatario, String token) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true"); // Encriptación TLS obligatoria
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(REMITENTE, PASSWORD_APP);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(REMITENTE));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(correo_destinatario));
+            
+            message.setSubject("Confirmación de cuenta y creación de Contraseña - Sistema de Gestión");
+            
+            String link = "http://localhost:8080/password/reset?token=" + token + "&id=" + id;
+            String contenido = "Hola,\n\nSe te a registrado como profesor de nuestra institucion. "
+                             + "Este mensaje es para confirmar su usuario dentro del sistema, además "
+                             + "debe crear una contraseña personal. Por favor, haz clic en el siguiente "
+                             + "enlace para crear una:\n\n"
+                             + link + "\n\nEste enlace expirará en 24 horas.";
+            
+            message.setText(contenido);
+
+            Transport.send(message);
+            System.out.println("Correo de recuperación enviado exitosamente a: " + correo_destinatario);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error crítico al intentar enviar el correo.");
+        }
+    }
+}
